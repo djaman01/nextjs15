be61d408895eff187d416c65dc598d37cd3563b3
Jaafar Bendahou <jaafar.bendahou@gmail.com>
2025-03-08 23:30:09 +0000

    pettirer + eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,14 +1,42 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
+import js from "@eslint/js";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
 
 const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
+const __dirname = path.dirname(__filename);
 const compat = new FlatCompat({
   baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
 });
 
-const eslintConfig = [...compat.extends("next/core-web-vitals")];
-
-export default eslintConfig;
+export default [
+  ...compat.extends(
+    "next/core-web-vitals",
+    "standard",
+    "plugin:tailwindcss/recommended",
+    "prettier"
+  ),
+  {
+    parserOptions: {
+      ecmaVersion: 2020,
+      sourceType: "module",
+    },
+    env: {
+      es6: true,
+      node: true,
+    },
+    plugins: ["import"],
+    settings: {
+      "import/resolver": {
+        node: {
+          extensions: [".js", ".jsx", ".ts", ".tsx"],
+        },
+      },
+    },
+    rules: {
+      "no-undef": "off",
+    },
+  },
+];
